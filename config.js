943eafd037b350494e37949e0d0825c1ffa4d807
imeshkawshalya <155098034+imeshkawshalya@users.noreply.github.com>
2024-12-04 11:08:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session is",  //session id dhan
+SESSION_ID: process.env.SESSION_ID || "iM5AibSZ#xGg06YjoGzJXbr0i7z_hiw5Ztz65EeKn3W2IHZVNprg",  //session id dhan
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/2qD0Syx/6648.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94774609569",

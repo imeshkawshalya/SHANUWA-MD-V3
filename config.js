5ee4c08399eb74d35e41f67b6f380e6a35c2eaaf
imeshkawshalya <155098034+imeshkawshalya@users.noreply.github.com>
2024-12-04 12:25:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW...
 SUDO_NB: process.env.SUDO_NB || "94774609569",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"false"
 };
 
